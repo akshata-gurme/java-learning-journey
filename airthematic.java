9d737a0c4138940f285d229f821cf397ea6cf434
akshata-gurme <akshatagurme101@gmail.com>
2026-09-01 21:20:19 +0530

    add arithematic operations program

---
+++ airthematic.java
@@ -0,0 +1,14 @@
+class airthematic{
+	public static void main(String[]args){
+	int num1=23;
+	int num2=45;
+	
+	
+	System.out.println("sum=" + (num1+num2));
+	System.out.println("sub=" +(num2-num1));
+	System.out.println("mult="+(num1*num2));
+	System.out.println("div="+(num1/num2));
+	System.out.println("mod="+(num1%num2));
+
+	}
+}
