5de8dc1e43c05520d4316a792ad8d5d5040822ac
akshata-gurme <akshatagurme101@gmail.com>
2026-09-01 23:05:59 +0530

    add total avg percentage

---
+++ marks.java
@@ -0,0 +1,22 @@
+class marks{
+	public static void main(String[] args){
+	int sub1=87;
+	int sub2=67;
+	int sub3=98;
+	int sub4=76;
+	int sub5=56;
+	int total=sub1+sub2+sub3+sub4+sub5;
+	int average=(sub1+sub2+sub3+sub4+sub5)/5;
+	double percentage=(total/500)*100;
+
+System.out.println("sub1 marks="+sub1);
+System.out.println("sub2 marks="+sub2);
+System.out.println("sub3 marks="+sub3);
+System.out.println("sub4 marks="+sub4);
+System.out.println("sub5 marks="+sub5);
+System.out.println("total marks="+total);
+System.out.println("average="+average);
+System.out.println("percentage =" +percentage);
+
+	}
+}
