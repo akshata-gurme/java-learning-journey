1269bc21dc5b54a4ac4644f1aba794b66eaa52af
akshata-gurme <akshatagurme101@gmail.com>
2026-09-01 22:31:24 +0530

    area of rectangle

---
+++ area.java
@@ -0,0 +1,12 @@
+class area{
+	public static void main(String[] args)
+	{
+	int length=12;
+	int breadth=88;
+	 int area=length*breadth;
+	 System.out.println("length = "+length);
+	 System.out.println("breadth = "+breadth);
+	 System.out.println("area of recctangle = "+area);
+
+	}
+}
