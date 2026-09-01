c8fab991b3b28a7a76b80800f2428ce6716fc5cd
akshata-gurme <akshatagurme101@gmail.com>
2026-09-01 22:07:23 +0530

    perimeter of rectangle

---
+++ perimeter.java
@@ -0,0 +1,15 @@
+public class perimeter{
+    public static void main(String[] args) {
+        // Define hardcoded values for length and breadth
+        double length = 10.5;
+        double breadth = 5.2;
+
+        // Formula: Perimeter = 2 * (length + breadth)
+        double perimeter = 2 * (length + breadth);
+
+        // Display results
+        System.out.println("Length: " + length);
+        System.out.println("Breadth: " + breadth);
+        System.out.println("Perimeter of the rectangle = " + perimeter);
+    }
+}
