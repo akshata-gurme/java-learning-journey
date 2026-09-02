2fa5080e63455c6fdd4135f8df23f48d6218b597
akshata-gurme <akshatagurme101@gmail.com>
2026-09-02 12:32:26 +0530

    calculating third angle

---
+++ angle.java
@@ -0,0 +1,11 @@
+class angle{
+	public static void main(String[]args){
+	int angle1=60;
+	int angle2=10;
+	double calculate=180-(angle1+angle2);
+
+	 System.out.println("angle1="+angle1);
+	 System.out.println("angle2=" +angle2);
+     System.out.println("calculate="+calculate);
+	}
+}
