61339fb2c3a6c79c172beaa484d04acac0090fa1
akshata-gurme <akshatagurme101@gmail.com>
2026-09-02 18:15:07 +0530

    circle area diameter and circumference

---
+++ circle.java
@@ -0,0 +1,16 @@
+class circle
+{
+	public static void main(String[]args){
+      int radius=2;
+      double diameter=2*radius;
+      double circumference=2*3.14*radius;
+      double area=3.14*radius*radius;
+
+      System.out.println("circle radius="+ radius);
+      System.out.println("circle diameter=" + diameter);
+      System.out.println("circle circumference="+circumference );
+      System.out.println("circle area="+area);
+
+
+	}
+}
