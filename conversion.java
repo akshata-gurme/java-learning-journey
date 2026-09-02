098ed592cd5cad84627d1e21da4df899c245204e
akshata-gurme <akshatagurme101@gmail.com>
2026-09-02 18:31:21 +0530

    conversion of meter centimeter and kilometer

---
+++ conversion.java
@@ -0,0 +1,13 @@
+class conversion{
+	public static void main(String[]args){
+	double cm=250000.00;
+	double m=cm/100;
+	double kilometers=cm/100000;
+
+
+	 System.out.println("cm="+cm);
+	 System.out.println("m=" +m);
+	 System.out.println("kilometers="+ kilometers);
+       
+	}
+}
