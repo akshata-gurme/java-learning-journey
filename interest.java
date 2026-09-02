541b0132dda78af3668d52f822119ed71b04e5a0
akshata-gurme <akshatagurme101@gmail.com>
2026-09-02 18:43:49 +0530

    calculating simple interest

---
+++ interest.java
@@ -0,0 +1,15 @@
+class interest{
+	public static void main(String[]args)
+{
+	int p=34;
+	int t=35;
+	int r=23;
+
+	double interest=(p*t*r)/100;
+
+	System.out.println("p="+p);
+	System.out.println("t="+t);
+	System.out.println("r="+r);
+	System.out.println("interest="+interest);
+
+}}
